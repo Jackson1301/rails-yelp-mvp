ff86435af225c1fbba5bd4d0076c734d1dcf291b
Jackson Cheung <jacksoncscheung@gmail.com>
2018-07-25 14:07:44 +0800

    kill

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+// app/assets/javascripts/application.js
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+// = require bootstrap-sprockets
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# get "restaurants", to: "restaurants#index"
+# get "restaurants/new", to: "restaurants#new"
+# get "restaurants/:id", to: "restaurants#show", as: :restaurant
+# post "restaurants", to: "restaurants#create"
+# get "restaurants/:id/reviews/new", to: "restaurants#new_review", as: :new_review
+# post "restaurants/:id/reviews", to: "restaurants#reviews", as: :reviews
+
+  resources :restaurants,  only: [ :index, :show, :new, :create] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
+
